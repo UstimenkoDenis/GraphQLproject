3f84877221711681eb0d5af368903d950099e0ff
Denis <ustimenkodenis@mail.ru>
2020-06-29 00:57:02 +0300

    transfer to MongoDB

--- schema/schema.js
+++ schema/schema.js
@@ -2,23 +2,41 @@ const graphql = require('graphql');
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLInt, GraphQLID, GraphQLList } = graphql;
 
-const movies = [
-    { id: '1', name: 'Pulp Fiction', genre: 'Crime', directorId: '1' },
-    { id: '2', name: '1984', genre: 'Sci-Fi', directorId: '2' },
-    { id: '3', name: 'V for vendetta', genre: 'Sci-Fi-Triller', directorId: '3' },
-    { id: '4', name: 'Snach', genre: 'Crime-Comedy', directorId: '4' },
-    { id: '5', name: 'Reservoir Dogs', genre: 'Crime', directorId: '1' },
-    { id: '6', name: 'The Hateful Eight', genre: 'Crime', directorId: '1' },
-    { id: '7', name: 'Inglourious Basterds', genre: 'Crime', directorId: '1' },
-    { id: '7', name: 'Lock, Stock, and Two Smoking Barrels', genre: 'Crime-Comedy', directorId: '4' },
-];
+// const moviesJson = [
+//     { "name": "Pulp Fiction", "genre": "Crime", "directorId": },
+//     { "name": "1984", "genre": "Sci-Fi", "directorId": },
+//     { "name": "V for vendetta", "genre": "Sci-Fi-Triller", "directorId": },
+//     { "name": "Snach", "genre": "Crime-Comedy", "directorId": },
+//     { "name": "Reservoir Dogs", "genre": "Crime", "directorId": },
+//     { "name": "The Hateful Eight", "genre": "Crime", "directorId": },
+//     { "name": "Inglourious Basterds", "genre": "Crime", "directorId": },
+//     { "name": "Lock, Stock, and Two Smoking Barrels", "genre": "Crime-Comedy", "directorId": },
+// ];
 
-const directors = [
-    { id: '1', name: 'Quentin Tarantino', age: 55 },
-    { id: '2', name: 'Michael Radford', age: 72 },
-    { id: '3', name: 'James McTeigue', age: 51 },
-    { id: '4', name: 'Guy Ritchie', age: 50 },
-];
+// const directorsJson = [
+//     {"name": "Quentin Tarantino", "age": 55 },  // 5ef90dbae092c32eb84fe826
+//     {"name": "Michael Radford", "age": 72 },  //   5ef90f6fe092c32eb84fe828
+//     {"name": "James McTeigue", "age": 51 },  //    5ef90fa7e092c32eb84fe829
+//     {"name": "Guy Ritchie", "age": 50 },  //       5ef90fd4e092c32eb84fe82a 
+// ];
+
+// const movies = [
+//     { id: '1', name: 'Pulp Fiction', genre: 'Crime', directorId: '1' },
+//     { id: '2', name: '1984', genre: 'Sci-Fi', directorId: '2' },
+//     { id: '3', name: 'V for vendetta', genre: 'Sci-Fi-Triller', directorId: '3' },
+//     { id: '4', name: 'Snach', genre: 'Crime-Comedy', directorId: '4' },
+//     { id: '5', name: 'Reservoir Dogs', genre: 'Crime', directorId: '1' },
+//     { id: '6', name: 'The Hateful Eight', genre: 'Crime', directorId: '1' },
+//     { id: '7', name: 'Inglourious Basterds', genre: 'Crime', directorId: '1' },
+//     { id: '7', name: 'Lock, Stock, and Two Smoking Barrels', genre: 'Crime-Comedy', directorId: '4' },
+// ];
+
+// const directors = [
+//     { id: '1', name: 'Quentin Tarantino', age: 55 },
+//     { id: '2', name: 'Michael Radford', age: 72 },
+//     { id: '3', name: 'James McTeigue', age: 51 },
+//     { id: '4', name: 'Guy Ritchie', age: 50 },
+// ];
 
 const MovieType = new GraphQLObjectType({
     name: 'Movie',
