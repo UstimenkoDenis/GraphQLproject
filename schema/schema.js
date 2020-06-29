01ae2bb212094180c08269624962b4140094cbb5
Denis <ustimenkodenis@mail.ru>
2020-06-29 21:54:13 +0300

    Mongoose added

--- schema/schema.js
+++ schema/schema.js
@@ -2,6 +2,9 @@ const graphql = require('graphql');
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLInt, GraphQLID, GraphQLList } = graphql;
 
+const Movies = require('../models/movie');
+const Directors = require('../models/director');
+
 // const moviesJson = [
 //     { "name": "Pulp Fiction", "genre": "Crime", "directorId": },
 //     { "name": "1984", "genre": "Sci-Fi", "directorId": },
@@ -47,7 +50,8 @@ const MovieType = new GraphQLObjectType({
         director: {
             type: DirectorType, 
             resolve(parent,args){
-                return directors.find(director => director.id == parent.directorId)
+                // return directors.find(director => director.id == parent.directorId)
+                return Directors.findById(parent.directorId);
             } 
         }
     }),
@@ -62,7 +66,8 @@ const DirectorType = new GraphQLObjectType({
         movies: {
             type: new GraphQLList(MovieType),
             resolve(parent, args) {
-                return movies.filter(movie => movie.directorId === parent.id)
+                // return movies.filter(movie => movie.directorId === parent.id)
+                return Movies.find({ directorId: parent.id});
             }
         }
     }),
@@ -75,26 +80,30 @@ const Query = new GraphQLObjectType({
             type: MovieType,
             args: { id: { type: GraphQLID} },
             resolve(parent, args) {
-                return movies.find(movie => movie.id == args.id);
+                // return movies.find(movie => movie.id == args.id);
+                return Movies.findById(args.id)
             }
         },
         director: {
             type: DirectorType,
             args: { id: { type: GraphQLID} },
             resolve(parent, args) {
-                return directors.find(director => director.id == args.id);
+                // return directors.find(director => director.id == args.id);
+                return Directors.findBiId(args.id)
             }
         },
         movies: {
             type: new GraphQLList(MovieType),
             resolve(parent, args) {
-                return movies;
+                // return movies;
+                return Movies.find({});
             }
         },
         directors: {
             type: new GraphQLList(DirectorType),
             resolve(parent, args) {
-                return directors;
+                // return directors;
+                return Directors.find({});
             }
         }
     }
