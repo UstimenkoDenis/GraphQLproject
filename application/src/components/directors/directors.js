684995e79ee50dca0aac11f97b8b67d1038b4d2b
Denis <ustimenkodenis@mail.ru>
2020-07-09 11:09:38 +0300

    directors table added

--- application/src/components/directors/directors.js
+++ application/src/components/directors/directors.js
@@ -10,12 +10,33 @@ class Directors extends Component {
 
         return (
             <div className="directors">
-                <h1>Directors</h1>
+                {/* <h1>Directors</h1>
                 <ul>
                     {   directors.map((director, i) => {
                             return ( <li key={director.id}>{director.name + " (" + director.age + " years old)"}</li>)
                         }) }
-                </ul>                
+                </ul>                 */}
+                 <table>
+                    <caption>Directors</caption>
+                    <thead>
+                        <tr>
+                            <td>Name</td><td>Age</td><td>Movies</td>
+                        </tr>
+                    </thead>
+                                       
+                    <tbody>
+                            {directors.map((director, i) => {
+                                    return ( 
+                                        <tr key={director.id}>
+                                            <td>{director.name}</td><td>{director.age}</td>
+                                            <td>{director.movies.map((movie, j) => {
+                                                    return ( <tr><td>{movie.name}</td></tr>)
+                                                })}</td> 
+                                        </tr>
+                                    )
+                                }) }
+                    </tbody>                    
+                </table>
             </div>
         )              
     }
